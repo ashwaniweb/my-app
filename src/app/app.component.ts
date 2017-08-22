b1aad16049526ec9c44b3d7b338fa4bd8780eb8b
ashwani kumar <ashwani.kumar@asergis.in>
2017-08-22 18:51:23 +0530

    caca

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  myData: Array<any>;
   title = 'app';
+
+  constructor(private http:Http) {
+    
+    this.http.get('/assets/data/photos.json')
+      .map(response => response.json())
+      .subscribe(res => this.myData = res);
+
+  }
 }
